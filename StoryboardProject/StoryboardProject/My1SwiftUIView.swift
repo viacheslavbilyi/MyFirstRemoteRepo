d77bd2f2b77669ec641032ed0f79baf345eb4d83
DevFork <DevFork@gmail.com>
2023-08-31 20:39:11 -0400

    added swiftui view

---
+++ StoryboardProject/StoryboardProject/My1SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  My1SwiftUIView.swift
+//  StoryboardProject
+//
+//  Created by Viacheslav Bilyi on 2023-08-31.
+//
+
+import SwiftUI
+
+struct My1SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct My1SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        My1SwiftUIView()
+    }
+}
